1dde7cf04e65598fb3521bd170d6c485ff6a8e5f
Eric Bishard <ebishard@progress.com>
2019-03-14 15:48:47 -0700

    step one: HTML, CSS, KendoReact & bootstrap-4-grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bootstrap-4-grid/css/grid.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="bootstrap-wrapper">
+        <div className="app-container container">
+          <div className="row">
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+              <h1>Sales | Q4 2018</h1>
+            </div>
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+              <button>Share</button>
+              <button>Export to PDF</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+              <h4>Panel Bar Container</h4>
+            </div>
+            <div className="col-xs-9 col-sm-9 col-md-9 col-lg-9 col-xl-9">
+              <div className="row">
+                <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                  <h4>Donut Chart Container</h4>
+                </div>
+                <div className="col-xs-6 col-sm-6 col-md-2 col-lg-2 col-xl-2">
+                  <div className="percentage-container">
+                    <span className="percentage-number">94</span>
+                    <span className="percentage-sign">%</span>
+                    <p>CUSTOMER SATISFACTION</p>
+                  </div>
+                  <div className="percentage-container">
+                    <span className="percentage-number">89</span>
+                    <span className="percentage-sign">%</span>
+                    <p>TARGET SALES</p>
+                  </div>
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                  <h4>Bar Chart Container</h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                  <h4>Grid Container</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h4 style={{ display: 'none' }}>Dialog Shown/Hidden with Logic</h4>
+        </div>
       </div>
     );
   }
